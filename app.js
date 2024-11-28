a1fb7973dd12aa4c59960407812e859d8938e857
Abhishan999 <148634703+Abhishan999@users.noreply.github.com>
2024-11-28 10:02:50 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const App = () => (
+  <Auth0Provider
+    domain="dev-jtrtkaayf3jznasd.us.auth0.com"
+    clientId="rD7Xx8R2q7XXD4iJNYkShOikPXRpI4Io"
+    authorizationParams={{
+      redirect_uri: https://sansthayein-aur-samvidhan-gucg7a0xl-abhishan999s-projects.vercel.app,
+    }}
+  >
+    <Sansthayein-aur-samvidhan>
+  </Auth0Provider>
+);
